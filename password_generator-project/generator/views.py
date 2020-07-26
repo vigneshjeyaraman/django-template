4be98fb1627cbf3144c338cf94ef88cf1477f8a0
Vignesh-Jeyaraman <vignesh.jeyaraman@algoworks.com>
2020-07-26 19:06:14 +0530

    password generator.

---
+++ password_generator-project/generator/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+import random
+# Create your views here.
+
+
+def home(request):
+    return render(request, 'generator/home.html')
+    
+def password(request):
+    characters = list('abcdefghijklmnopqrstuvwxyz')
+    length = int(request.GET.get('length', 12))
+    if request.GET.get('uppercase'):
+        characters.extend(list('ABCDEFGHIJKLMNOPQRSTUVWXYZ'))
+    if request.GET.get('special'):
+        characters.extend(list('!@#$%^&*()'))
+    if request.GET.get('numbers'):
+        characters.extend('0123456789')
+    thepassword = ''
+    for x in range(length):
+        thepassword += random.choice(characters)
+    return render(request, 'generator/password.html', {'password': thepassword})
+
+def about(request):
+    return render(request, 'generator/about.html')
